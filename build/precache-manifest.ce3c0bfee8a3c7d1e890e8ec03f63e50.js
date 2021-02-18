a5f08e82043612887489f31c7cec8e01f86ff1ce
komaldevi <devi0087@algonquinlive.com>
2021-02-18 12:53:59 -0500

    first commit

---
+++ build/precache-manifest.ce3c0bfee8a3c7d1e890e8ec03f63e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "732825ab8ec682f7434e",
+    "url": "/static/css/main.37baba22.chunk.css"
+  },
+  {
+    "revision": "732825ab8ec682f7434e",
+    "url": "/static/js/main.00b632d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d070571468b7f3431734",
+    "url": "/static/js/2.0a933db9.chunk.js"
+  },
+  {
+    "revision": "2972c13a36d8a025ff36d21aa2bcfd6f",
+    "url": "/static/media/flower.2972c13a.jpg"
+  },
+  {
+    "revision": "aa4d9c4b907ddcb8550a9c38521e5e31",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Reveal from "./Components";
+
+function App() {
+  return (
+    <div className="App">
+      <Reveal />
+    </div>
+  );
+}
+
+export default App;
